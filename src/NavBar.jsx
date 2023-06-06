cf126ed3c74330475641ffb2b025c43df9eb0397
Taffie Theuri <taffietheuri4@gmail.com>
2023-06-06 09:43:56 +0300

    Update navigation bar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -5,9 +5,10 @@ import { NavLink } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div>
-        <NavLink exact to="/" className="nav-button"> My Events </NavLink>
+
         <NavLink exact to="/" className="nav-button"> New Event </NavLink>
         <NavLink exact to="/" className="nav-button"> New Venue </NavLink>
+        <NavLink exact to="/" className="nav-button"> My Events </NavLink>
         <NavLink to='/' className="nav-button">Artist Stats</NavLink>
     </div>
   )
