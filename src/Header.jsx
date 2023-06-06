a625bf59e1fceef8e83eeacf585e19984cfe40bc
Taffie Theuri <taffietheuri4@gmail.com>
2023-06-06 09:45:45 +0300

    Update Header section

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+
+export default function Header() {
+  return (
+    <>
+    <header>
+      <h1>event.ager</h1>
+      <NavBar />
+    </header>
+    </>
+  )
+}
