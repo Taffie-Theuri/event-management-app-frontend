d02290b6ae94cf6b5c6233761e61ef469eb3be27
Taffie Theuri <taffietheuri4@gmail.com>
2023-06-07 11:41:15 +0300

    Create event list component

---
+++ src/EventList.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import EventCard from './EventCard';
+import Search from './Search';
+
+function EventList({data, handleDeleteEvent, handleUpdateEvent}) {
+  const [dataIndex, setDataIndex] = useState(0)
+
+
+  const [searchTerm, setSearchTerm] = useState("")
+
+
+//TBD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+  const eventList = [...data]
+  .filter((data) => {
+    return data.artist.name.toLowerCase().includes(searchTerm.toLowerCase());
+  })
+    .slice(dataIndex, dataIndex + 8)
+
+    .map((data) =>
+    <EventCard
+        key={data.id}
+        data={data}
+        handleDeleteEvent={handleDeleteEvent}
+        handleUpdateEvent={handleUpdateEvent}
+    />)
+
+    function handleClickMore() {
+      setDataIndex((dataIndex) => (dataIndex + 8) % data.length);
+    }
+
+  return (
+    <>
+    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+    <div className="event-list">
+    {eventList}
+    </div>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <div className="next-container">
+        <button className="next-button" onClick={handleClickMore}>Next</button>
+    </div>
+    </>
+  )
+}
+
+export default EventList;
