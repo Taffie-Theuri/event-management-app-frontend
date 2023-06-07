19c4da7efceec27e8becd3ca391b97b3d1997b14
Taffie Theuri <taffietheuri4@gmail.com>
2023-06-07 11:43:55 +0300

    event container component update

---
+++ src/EventContainer.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+import EventList from './EventList';
+
+
+function EventContainer({data, handleDeleteEvent, handleUpdateEvent}) {
+
+    return(
+    <>
+    <br/>
+    <h2>My Events</h2>
+    <br/>
+        <EventList
+        data={data}
+        handleDeleteEvent={handleDeleteEvent}
+        handleUpdateEvent={handleUpdateEvent}
+        />
+    </>
+    )
+}
+
+export default EventContainer;
