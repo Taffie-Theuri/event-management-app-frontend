0baf5ee34de130f9c3ea7e0e8084753d8bbf6fb9
Samuel <samnmbuguah@gmail.com>
2023-06-07 15:02:15 +0300

    Fixed Navbar issues

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div>
-
-        <NavLink exact to="/" className="nav-button"> New Event </NavLink>
-        <NavLink exact to="/" className="nav-button"> New Venue </NavLink>
-        <NavLink exact to="/" className="nav-button"> My Events </NavLink>
-        <NavLink to='/' className="nav-button">Artist Stats</NavLink>
+      <NavLink exact to="/" className="nav-button">
+        New Event
+      </NavLink>
+      <NavLink exact to="/create-venue" className="nav-button">
+        New Venue
+      </NavLink>
+      <NavLink exact to="/my-events" className="nav-button">
+        My Events
+      </NavLink>
+      <NavLink exact to="/stats" className="nav-button">
+        Artist Stats
+      </NavLink>
     </div>
-  )
+  );
 }
